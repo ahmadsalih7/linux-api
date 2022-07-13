49a9c5d18ac95be8d077f8e800ef884f3639c629
Ahmad Hasan <ahmadsalih7@hotmail.com>
2022-07-13 09:29:05 +0300

    Use host address from configuration file

--- index.js
+++ index.js
@@ -5,8 +5,9 @@ const logger = require("./logger/dev_logger");
 require("dotenv").config();
 
 const port = config.get("app.port");
+const host = config.get("app.host");
 
 mongoose_connect();
 app.listen(port, () => {
-  logger.info(`Connected to port ${port} ...`);
+  logger.info(`Server is running on http://${host}:${port} ...`);
 });
